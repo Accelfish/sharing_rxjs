5b16975a446956d4d06cec0b2d9d07204904d97a
Adam_Yu <adam_yu@cmoney.com.tw>
2022-11-17 10:54:06 +0800

    modify: 加強 autoComplete、畫圖加上註解

--- draw.js
+++ draw.js
@@ -1,6 +1,6 @@
 const { fromEvent } = rxjs;
 // https://rxjs.dev/api/operators
-const { takeUntil, flatMap, tap } = rxjs.operators;
+const { takeUntil, mapTo, flatMap, tap } = rxjs.operators;
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -9,11 +9,11 @@ canvas.height = 600;
 
 ctx.beginPath(); // 開始畫畫
 
-function draw(e){
-    ctx.lineTo(e.clientX,e.clientY); // 移到滑鼠在的位置
-    ctx.stroke(); // 畫畫
-}
-
+// function draw(e){
+//     ctx.lineTo(e.clientX,e.clientY); // 移到滑鼠在的位置
+//     ctx.stroke(); // 畫畫
+// }
+//
 // // 按下去滑鼠才開始偵測 mousemove 事件
 // canvas.addEventListener('mousedown', function(e){
 //     ctx.moveTo(e.clientX, e.clientY);
@@ -25,7 +25,31 @@ function draw(e){
 //     canvas.removeEventListener('mousemove', draw);
 // })
 
+// step1 將滑鼠按下事件轉成Observerable
+fromEvent(canvas, 'mousedown')
+    .subscribe(e => draw(e))
 
+// step2 滑鼠按下事件之後要監聽移動事件
+fromEvent(canvas, 'mousedown').pipe(
+        mapTo(fromEvent(canvas, 'mousemove'))
+    )
+    .subscribe(e => {
+        console.log(e);
+        draw(e)
+    })
+
+// step3 移動事件要取的是 Observable 的東西 [1, [2,3,4],5,[6,7,8]...]
+// 所以....壓平它，變成只有一條的事件流
+fromEvent(canvas, 'mousedown').pipe(
+        flatMap(e => fromEvent(canvas, 'mousemove'))
+    )
+    .subscribe(e => {
+        console.log(e);
+        // draw(e)
+    })
+
+// step4 處理一下移動位置 使用tap 這個不影響 stream 的 operator
+// 再處理一下移動時直到滑鼠放開的事件，停止 subscribe，使用 takeUntil
 fromEvent(canvas, 'mousedown').pipe(
     tap(e => ctx.moveTo(e.clientX, e.clientY))
         ,flatMap(e => fromEvent(canvas, 'mousemove').pipe(
--- search.js
+++ search.js
@@ -1,10 +1,11 @@
-const baseUrl = `https://api.github.com/search/repositories`
-const { fromEvent, interval } = rxjs;
+const baseUrl = `https://api.github.com/search/repositories`;
+const { fromEvent, interval, of } = rxjs;
 // https://rxjs.dev/api/operators
 const { debounceTime, map, debounce, tap, filter, scan, switchMap, distinctUntilChanged } = rxjs.operators;
 const { ajax } = rxjs.ajax;
 
-const searchInput = document.getElementById('searchInput');
+const DomSearchInput = document.getElementById('searchInput');
+const DomResultList = document.querySelector('#result');
 
 // fromEvent(searchInput, 'input').pipe(
 //         tap(() => console.log('input，但不影響stream'))
@@ -15,6 +16,13 @@ const searchInput = document.getElementById('searchInput');
 //     // send request
 //   })
 
+// render result
+renderList = (list) => {
+    DomResultList.innerHTML = "";
+    DomResultList.innerHTML=(list.map(item => '<li>' + item + '</li>'))
+}
+
+// call api，這裡用 rx 的 ajax 處理，把它變成 Observable
 getSuggestions = (keyword) => {
     const searchUrl = `${baseUrl}?q=${keyword}&per_page=10&page=1`;
     return ajax(searchUrl).pipe(
@@ -24,12 +32,22 @@ getSuggestions = (keyword) => {
     );
   };
 
-fromEvent(searchInput, 'input').pipe(
-    map(e => e.target.value)
-    ,debounceTime(3000)
+fromEvent(DomSearchInput, 'input').pipe(
+    map(e => DomSearchInput.value)
+    ,debounceTime(1000)
     ,distinctUntilChanged()
-    ,switchMap(keyword => getSuggestions(keyword))
-    
+    ,switchMap(keyword => keyword.length < 1 ? of([]) : getSuggestions(keyword))
 ).subscribe(suggestion => {
     console.log(suggestion);
+    // render function is here
+    renderList(suggestion);
 })
+
+fromEvent(DomResultList, 'click').pipe(
+        filter(e => e.target.matches('li'))
+        ,map(e => e.target.innerHTML)
+    )
+    .subscribe(value => {
+        DomSearchInput.value = value;
+        renderList([])
+    })
